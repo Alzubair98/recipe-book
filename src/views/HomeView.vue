59ef61467a1ca6533f71c495ea007383ab2f9a9d
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2024-12-15 08:52:24 +0300

    create and import pinina store

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -14,4 +14,8 @@
   </main>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useRecipeStore } from '../stores/recipe'
+
+const recipeStore = useRecipeStore()
+</script>
