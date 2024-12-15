ca3f492cdff0cba2202158ba69096ab20e7b9517
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2024-12-15 14:56:44 +0300

    add computed function for pinia

--- src/views/RecipeView.vue
+++ src/views/RecipeView.vue
@@ -1,33 +1,17 @@
 <template>
-  <div>Recipe {{ $route.params.id }}</div>
   <div>
-    <nav>
-      <ul>
-        <li>
-          <RouterLink :to="{ name: 'recipe', params: { id: 1 } }">Recipe 1</RouterLink>
-        </li>
-        <li>
-          <RouterLink :to="{ name: 'recipe', params: { id: 3 } }">Recipe 3</RouterLink>
-        </li>
-      </ul>
-    </nav>
+    <h1>{{ recipe?.name }}</h1>
+    <p>{{ recipe?.description }}</p>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { onMounted } from 'vue'
-import { RouterLink, useRoute } from 'vue-router'
-import { watch } from 'vue'
+import { useRoute } from 'vue-router'
+import { computed } from 'vue'
+import { useRecipeStore } from '../stores/recipe'
 
 const route = useRoute()
+const recipeStore = useRecipeStore()
 
-console.log(route.params)
-
-onMounted(() => console.log(`Fetching data inside onMounted`))
-
-watch(
-  () => route.params.id,
-  () => console.log('fetching inside watch'),
-  { immediate: true },
-)
+const recipe = computed(() => recipeStore.getRecipeById(route.params.id as string))
 </script>
