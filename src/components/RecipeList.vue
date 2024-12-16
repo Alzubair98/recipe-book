62f2312076b58fee60e106291b3423c398ee6b06
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2024-12-17 00:27:48 +0300

    create recipe comp

---
+++ src/components/RecipeList.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <nav v-if="recipes.length > 0">
+      <ul>
+        <li v-for="recipe in recipes" :key="recipe.id">
+          <router-link :to="{ name: 'recipe', params: { id: recipe.id } }">{{
+            recipe.name
+          }}</router-link>
+        </li>
+      </ul>
+    </nav>
+    <div v-else>No recipes found!</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { Recipe } from '../stores/recipe'
+defineProps<{
+  recipes: Recipe[]
+}>()
+</script>
--- src/stores/recipe.ts
+++ src/stores/recipe.ts
@@ -1,7 +1,7 @@
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
 
-interface Recipe {
+export interface Recipe {
   id: string
   name: string
   description: string
