8ba863628c9ee157f8e61c02904dc2ed6c75169b
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2024-12-16 08:36:52 +0300

    add search bar

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,20 +1,33 @@
 <template>
   <main>
-    <h1>Home</h1>
-    <nav v-if="recipeStore.recipes.length > 0">
+    <h1>Recipe Book</h1>
+    <div>
+      <input type="text" placeholder="search recipes..." v-model="searchQuery" />
+    </div>
+    <nav v-if="filteredRecipes.length > 0">
       <ul>
-        <li v-for="recipe in recipeStore.recipes" :key="recipe.id">
+        <li v-for="recipe in filteredRecipes" :key="recipe.id">
           <router-link :to="{ name: 'recipe', params: { id: recipe.id } }">{{
             recipe.name
           }}</router-link>
         </li>
       </ul>
     </nav>
+    <div v-else>No recipes found!</div>
   </main>
 </template>
 
 <script setup lang="ts">
+import { computed, ref } from 'vue'
 import { useRecipeStore } from '../stores/recipe'
 
 const recipeStore = useRecipeStore()
+const searchQuery = ref('')
+
+// functions
+const filteredRecipes = computed(() =>
+  recipeStore.recipes.filter((recipe) =>
+    recipe.name.toLocaleLowerCase().includes(searchQuery.value.toLocaleLowerCase()),
+  ),
+)
 </script>
